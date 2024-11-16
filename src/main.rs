9e9c833734d202b082dcd913b179074fb0ea36e6
kanngji <alleneffort1994@gmail.com>
2024-11-17 01:55:23 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let my_number = 100;
+    let my_other_number = 200;
+    let third_number = my_number + my_other_number;
+    // type inference
+    
+    println!("{}", third_number);
+}

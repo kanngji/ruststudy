898650130d7393675f39490ed2e2b067663ad907
kanngji <alleneffort1994@gmail.com>
2024-11-19 01:10:18 +0900

    241119

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::mem::size_of;
+// 함수!
+fn give_age() -> i32 {
+    42
+}
+
 fn main() {
-    let my_number = 100;
-    let my_other_number = 200;
-    let third_number = my_number + my_other_number;
-    // type inference
-    
-    println!("{}", third_number);
+    //let my_number= u8 = 9;
+    let my_number2= 9_u8;
+    println!("{}",my_number2);
+
+    let num1 = 9.4;
+    let num2 = 9;
+    println!("{}", num1 as i32+num2);
+
+
+    // macro = function that writes code 
+    // 실행 cargo run
+
+    let my_name ="kang";
+    println!("MY name is {},{}",my_name,give_age());
 }
